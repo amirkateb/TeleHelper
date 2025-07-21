2a3f04e31534c03c3d6bb3ee869a154a95a2f6b8
amirkateb <amveks43@gmail.com>
2025-07-21 20:23:01 +0330

    Update Telegram.php

--- TelegramSender/src/Facades/Telegram.php
+++ TelegramSender/src/Facades/Telegram.php
@@ -5,6 +5,13 @@ namespace TeleHelper\TelegramSender\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * Facade for TelegramSender
+ *
+ * @method static \TeleHelper\TelegramSender\TelegramBot bot(string $name = null)
+ * @method static array listBots()
+ * @method static bool hasBot(string $name)
+ * @method static void addBot(string $name, string $token)
+ *
  * @see \TeleHelper\TelegramSender\TelegramSender
  */
 class Telegram extends Facade
