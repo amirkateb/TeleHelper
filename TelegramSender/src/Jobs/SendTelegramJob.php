e2366f9dfdbd581b4338afd1e23d0d5ecf1b5200
amirkateb <amveks43@gmail.com>
2025-07-21 20:32:16 +0330

    Update SendTelegramJob.php

--- TelegramSender/src/Jobs/SendTelegramJob.php
+++ TelegramSender/src/Jobs/SendTelegramJob.php
@@ -16,13 +16,15 @@ class SendTelegramJob implements ShouldQueue
     public string $botName;
     public string $method;
     public array $arguments;
+    public string $queue = 'default';
 
-    public function __construct(string $botName, string $method, array $arguments)
-    {
-        $this->botName = $botName;
-        $this->method = $method;
-        $this->arguments = $arguments;
-    }
+    public function __construct(string $botName, string $method, array $arguments, string $queue = 'default')
+{
+    $this->botName = $botName;
+    $this->method = $method;
+    $this->arguments = $arguments;
+    $this->queue = $queue;
+}
 
     public function handle(): void
     {
