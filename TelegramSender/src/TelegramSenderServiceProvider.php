d6ce528f5a4446ed1e35bc8faaa3e39fa2952396
amirkateb <amveks43@gmail.com>
2025-07-21 20:40:23 +0330

    Update TelegramSenderServiceProvider.php

--- TelegramSender/src/TelegramSenderServiceProvider.php
+++ TelegramSender/src/TelegramSenderServiceProvider.php
@@ -3,15 +3,12 @@
 namespace TeleHelper\TelegramSender;
 
 use Illuminate\Support\ServiceProvider;
+use TeleHelper\TelegramSender\Commands\SendTestTelegramCommand;
 
 class TelegramSenderServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
     public function register(): void
     {
-
         $this->mergeConfigFrom(
             __DIR__ . '/../config/telegram-sender.php',
             'telegram-sender'
@@ -22,13 +19,16 @@ class TelegramSenderServiceProvider extends ServiceProvider
         });
     }
 
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
         $this->publishes([
             __DIR__ . '/../config/telegram-sender.php' => config_path('telegram-sender.php'),
         ], 'telegram-sender-config');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                SendTestTelegramCommand::class,
+            ]);
+        }
     }
 }
