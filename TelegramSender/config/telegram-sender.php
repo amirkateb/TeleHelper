9d55d6afcfbf74be4f22e95a7676a09d558d93b5
amirkateb <amveks43@gmail.com>
2025-07-21 20:17:39 +0330

    Update telegram-sender.php

--- TelegramSender/config/telegram-sender.php
+++ TelegramSender/config/telegram-sender.php
@@ -9,14 +9,7 @@ return [
             'name' => 'default',
             'token' => env('TELEGRAM_BOT_TOKEN'),
         ],
-        [
-            'name' => 'marketing',
-            'token' => env('TELEGRAM_MARKETING_TOKEN'),
-        ],
-        [
-            'name' => 'support',
-            'token' => env('TELEGRAM_SUPPORT_TOKEN'),
-        ],
+        // Add more bots here...
     ],
 
 ];
